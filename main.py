4e29eece042de3a67ebe7388045cbfbacdb93751
jwietrzykowski <jan.m.wietrzykowski@gmail.com>
2022-02-17 12:42:27 +0100

    Komentarze i ocena.

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import Train
 import Detection_Classification
 
+# TODO Jakość kodu i raport (3.5/4)
+# TODO Raport malo przejrzysty.
+
+# TODO Skuteczność klasyfikacji 0.864 (4/4)
+# TODO [0.00, 0.50) - 0.0
+# TODO [0.50, 0.55) - 0.5
+# TODO [0.55, 0.60) - 1.0
+# TODO [0.60, 0.65) - 1.5
+# TODO [0.65, 0.70) - 2.0
+# TODO [0.70, 0.75) - 2.5
+# TODO [0.75, 0.80) - 3.0
+# TODO [0.80, 0.85) - 3.5
+# TODO [0.85, 1.00) - 4.0
+
+
+# TODO Skuteczność detekcji (0/2)
+
 TrainingData = Train.LoadTrainingData()
 Train.CreateBowVocab(TrainingData)
 Classifier = Train.Train_KNN_Clasifier(TrainingData)
